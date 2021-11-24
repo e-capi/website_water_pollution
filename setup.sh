d49d461a6abd23f39ca218fda596366a691f38eb
Emilio Capitaine <emiliocapitaine96@gmail.com>
2021-11-24 17:12:19 +0100

    trying to upload

--- setup.sh
+++ setup.sh
@@ -1,13 +1,17 @@
+pip install .
 mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"${emiliocapitaine96@gmail.com}\"\n\
+email = \"emiliocapitaine96@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
 [server]\n\
 headless = true\n\
-enableCORS = false\n\
+enableCORS=false\n\
 port = $PORT\n\
+[theme]\n\
+base=\"dark\"\n\
+primaryColor=\"#7c20a2\"\n\
 " > ~/.streamlit/config.toml
